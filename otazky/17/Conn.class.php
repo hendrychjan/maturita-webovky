8d4fdef90e2325179a404ae70b9587f4c9706108
hendrychjan <jan.honza.hendrych@gmail.com>
2022-05-17 12:43:47 +0200

    Otázka 17 - Regulární výrazy, datumové a řetězcové funkce

--- otazky/17/Conn.class.php
+++ otazky/17/Conn.class.php
@@ -1,6 +1,7 @@
 <?php
 
-class Conn {
+class Conn
+{
     public static function Connect()
     {
         return new PDO("mysql:host=localhost;port=3306;dbname=shop", "root", "", [
@@ -9,7 +10,65 @@ class Conn {
             PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
         ]);
     }
-    public static function Produkty() {
+    public static function Produkty()
+    {
         return self::Connect()->query("SELECT produkty.id, produkty.nazev, produkty.cena, kategorie.nazev kategorie FROM produkty JOIN kategorie ON produkty.kategorie = kategorie.id", PDO::FETCH_OBJ)->fetchAll();
+    }
+
+    // Formátovat datum tak, aby šel uložit do databáze
+    public static function fomatDatum($datum) {
+        $d = date("Y-m-d", strtotime($datum));
+        return $d;
+    }
+
+    // Validovat akci
+    public static function validateAkce($payload)
+    {
+        // Výsledek validace - obsahuje informaci o výsledku, pokud validace
+        // selže, vrátí i hlášení o chybách
+        $validationResult = array(
+            "valid" => true,
+            "validationErrors" => array(),
+        );
+
+        // Produkt - musí být validní id existujícího produktu
+        $produkty = self::Produkty();
+        if (!in_array($payload["produkt"], array_column($produkty, "id"))) {
+            $validationResult["valid"] = false;
+            array_push($validationResult["validationErrors"], "\"Produkt\" musí existovat");
+        }
+        
+        // Sleva (%) - rozsah v procentech od 0 do 100
+        $slevaRegex = '/^([0-9]{1,2}|100)$/';
+        if (!preg_match($slevaRegex, $payload["sleva"]) || $payload["sleva"] == 0) {
+            $validationResult["valid"] = false;
+            array_push($validationResult["validationErrors"], "\"Sleva\" musí být v rozsahu 1-100");
+        }
+
+        // Datum - platný datum ve formátu d.m.rrrr
+        $datumRegex = '/^([1-9]|[1-2][0-9]|3[0-1])\.([1-9]|1[0-2])\.[2-9][0-9]{3}$/';
+        if (!preg_match($datumRegex, $payload["datum"])) {
+            $validationResult["valid"] = false;
+            array_push($validationResult["validationErrors"], "\"Datum do\" musí být datum ve formátu d.m.rrrr");
+            return $validationResult; // nemá cenu dál pokračovat
+        }
+
+        // Datum - platé datum
+        $datum = explode(".", $payload["datum"]);
+        if (!checkdate($datum[1], $datum[0], $datum[2])) {
+            $validationResult["valid"] = false;
+            array_push($validationResult["validationErrors"], "\"Datum do\" není platné datum");
+            return $validationResult; // nemá cenu dál pokračovat
+        }
+    
+        // Datum - datum v budoucnosti
+        $dnes = date("Y-m-d");
+        $do = strtotime($datum[2] . "-" . $datum[1] . "-" . $datum[0]);
+        if ($do <= strtotime($dnes)) {
+            $validationResult["valid"] = false;
+            array_push($validationResult["validationErrors"], "\"Datum do\" musí být datum v budoucnosti");
+        }
+
+        return $validationResult;
     }
 }
--- otazky/17/vytvoreniAkce.php
+++ otazky/17/vytvoreniAkce.php
@@ -1,32 +1,51 @@
 <?php
-    spl_autoload_register(function ($className) {
-        require_once("{$className}.class.php");
-    });
+spl_autoload_register(function ($className) {
+    require_once("{$className}.class.php");
+});
+
+if (isset($_POST["submit"])) {
+    $validace = Conn::validateAkce($_POST);
+}
+
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>17</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
 <body>
-    <form>
-        <select>
-<?php
-    foreach (Conn::Produkty() as $p):
-?>
-            <option value="<?=$p->id?>"><?=$p->nazev?> (<?=$p->kategorie?>)</option>
-<?php
-    endforeach;
-?>
+    <form method="POST">
+        <select name="produkt">
+            <?php
+            foreach (Conn::Produkty() as $p) :
+            ?>
+                <option value="<?= $p->id ?>"><?= $p->nazev ?> (<?= $p->kategorie ?>)</option>
+            <?php
+            endforeach;
+            ?>
         </select>
-        <input type="text" placeholder="Sleva %" />
-        <input type="text" placeholder="Platí do" />
-        
-        <button type="submit">Poslat</button>
+        <input type="text" name="sleva" placeholder="Sleva %" />
+        <input type="text" name="datum" placeholder="Platí do" />
+
+        <button type="submit" name="submit">Poslat</button>
     </form>
-    
+
+    <?php
+    if (isset($validace)) {
+        if (!$validace["valid"]) {
+            foreach ($validace["validationErrors"] as $error) {
+                echo "<p>$error</p>";
+            }
+        } else {
+            echo "<p>Valid!</p>";
+            echo "<p><b>" . Conn::fomatDatum($_POST["datum"]) . "</b></p>";
+        }
+    }
+    ?>
 </body>
+
 </html>
